b25f5bede5d0b7f30b8508a3c3c7fe4125780f71
Jmrodrival04 <jvaldrod@myuax.com>
2024-05-13 15:30:28 +0200

    PruebaTecnicas

---
+++ src/main/java/Construction/RestaurantManager.java
@@ -0,0 +1,20 @@
+package Construction;
+public class RestaurantManager {
+    private static RestaurantManager instance;
+    private RestaurantFactory factory;
+
+    private RestaurantManager() {
+        factory = new RestaurantFactory();
+    }
+
+    public static synchronized RestaurantManager getInstance() {
+        if (instance == null) {
+            instance = new RestaurantManager();
+        }
+        return instance;
+    }
+
+    public Restaurant createRestaurant(String type) {
+        return factory.getRestaurant(type);
+    }
+}
--- src/main/java/Construction/ServiceStrategy.java
+++ src/main/java/Construction/ServiceStrategy.java
@@ -2,4 +2,24 @@ package Construction;
 
 public interface ServiceStrategy {
     void serve();
+    class CasualServiceStrategy implements ServiceStrategy {
+        @Override
+        public void serve() {
+            System.out.println("Casual service for fast food.");
+        }
+    }
+
+    class ThemedServiceStrategy implements ServiceStrategy {
+        @Override
+        public void serve() {
+            System.out.println("Thematic service with special decorations.");
+        }
+    }
+
+    class FineDiningServiceStrategy implements ServiceStrategy {
+        @Override
+        public void serve() {
+            System.out.println("Exquisite service with attention to detail.");
+        }
+    }
 }
