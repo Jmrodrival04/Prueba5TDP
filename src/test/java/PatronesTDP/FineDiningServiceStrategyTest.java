20668e450595766fc008c836a569fbbbb459f980
Jmrodrival04 <jvaldrod@myuax.com>
2024-05-13 16:24:08 +0200

    PruebaTecnicas

---
+++ src/test/java/PatronesTDP/FineDiningServiceStrategyTest.java
@@ -0,0 +1,12 @@
+package PatronesTDP;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FineDiningServiceStrategyTest {
+
+    @Test
+    void serve() {
+    }
+}
---
+++ src/test/java/PatronesTDP/RestaurantGroupTest.java
@@ -0,0 +1,20 @@
+package PatronesTDP;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantGroupTest {
+
+    @Test
+    void serveCustomers() {
+    }
+
+    @Test
+    void addRestaurant() {
+    }
+
+    @Test
+    void setServiceStrategy() {
+    }
+}
