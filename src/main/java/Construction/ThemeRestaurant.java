53ca4cbe1efe3146e0af6063aea1e2f094a7cd45
Jmrodrival04 <jvaldrod@myuax.com>
2024-05-13 15:29:07 +0200

    PruebaTecnicas

---
+++ src/main/java/Construction/ThemeRestaurant.java
@@ -0,0 +1,15 @@
+package Construction;
+
+public class ThemeRestaurant implements Restaurant {
+    private ServiceStrategy strategy;
+
+    @Override
+    public void serveCustomers() {
+        strategy.serve();
+    }
+
+    @Override
+    public void setServiceStrategy(ServiceStrategy strategy) {
+        this.strategy = strategy;
+    }
+}
